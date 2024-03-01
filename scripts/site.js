d6805e1bebd9597476a8c89b9fd68c237e1c93e9
“Evertech <evertonrichards0@gamil.com>
2024-03-01 11:01:20 -0500

    layout fixed with bootstrap

--- scripts/site.js
+++ scripts/site.js
@@ -1,3 +1,19 @@
 function ClearForm(){
     alert('You click clear function external');
 }
+
+$(function(){
+    var pages = ['index', 'about', 'contact'];
+    var pathname = window.location.pathname;
+
+    $('.nav-link').each(function (item) {
+        if(pathname.includes(pages[item])) {
+
+            $(this).addClass('active');
+            $(this).attr('aria-current', 'page');
+
+        }
+    });
+
+
+});
