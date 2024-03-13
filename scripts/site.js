8b9304111c179c8e4bc3c0e40401dd5ceeb1d588
“Evertech <evertonrichards0@gamil.com>
2024-03-13 10:59:45 -0500

    Dashboards finished

--- scripts/site.js
+++ scripts/site.js
@@ -1,5 +1,6 @@
 function ClearForm() {
-    alert('You click clear function external');
+    alert('Thank you for your feedback');
+    
 }
 function petition() {
     alert('Thank you for voting');
@@ -20,6 +21,22 @@ $(function () {
     });
 
 });
+
+//////// Hide and Show Divs in Dashboard //////////////////
+function hideSection(section) {
+   
+    const sections = ['dashboard', 'grades', 'report', 'attendance', 'comments', 'fees', 'petition', 'suggestion'];
+
+
+    for (let i = 0; i < sections.length; i++) {
+        const currentSection = document.getElementById(sections[i]);
+        if (sections[i] !== section) {
+            currentSection.setAttribute("hidden", true);
+        } else {
+            currentSection.removeAttribute("hidden");
+        }
+    }
+}
 
 
 ///////create a signature/////////////////////////////////////////
@@ -63,135 +80,124 @@ function clearCanvas() {
 ////////////////////////////////////////////////////////////////////////////
 
 
-//////// Hide and Show Divs in Dashboard //////////////////
-function hideSection(section) {
-    const dashboard = document.getElementById('dashboard');
-    const grade = document.getElementById('grades');
-    const report = document.getElementById('report');
-    const attendance = document.getElementById('attendance');
-    const comments = document.getElementById('comments');
-    const fees = document.getElementById('fees');
-    const petition = document.getElementById('petition');
-    const suggestion = document.getElementById('suggestion');
 
 
-    // Add click event listener to the toggleDiv
-    // toggleDiv.addEventListener('click', function () {
-    // Toggle visibility of firstDiv and secondDiv
+//     const dashboard = document.getElementById('dashboard');
+//     const grade = document.getElementById('grades');
+//     const report = document.getElementById('report');
+//     const attendance = document.getElementById('attendance');
+//     const comments = document.getElementById('comments');
+//     const fees = document.getElementById('fees');
+//     const petition = document.getElementById('petition');
+//     const suggestion = document.getElementById('suggestion');
 
 
-    switch (section) {
-        case 'dashboard':
-            grade.style.display = 'none';
-            report.style.display = 'none';
-            // attendance.style.display = 'none';
-            // comments.style.display = 'none';
-            // fees.style.display = 'none';
-            // petition.style.display = 'none';
-            // suggestion.style.display = 'none';
-            dashboard.style.display = 'block';
-
-            break;
-        case 'grades':
-
-            report.style.display = 'none';
-            // attendance.style.display = 'none';
-            // comments.style.display = 'none';
-            // fees.style.display = 'none';
-            // petition.style.display = 'none';
-            // suggestion.style.display = 'none';
-            dashboard.style.display = 'none';
-            grade.style.display = 'block';
-            // code to execute if expression === value1
-            break;
-        case 'report':
-            grade.style.display = 'none';
-            // attendance.style.display = 'none';
-            // comments.style.display = 'none';
-            // fees.style.display = 'none';
-            // petition.style.display = 'none';
-            // suggestion.style.display = 'none';
-            dashboard.style.display = 'none';
-            report.style.display = 'block';
-            // code to execute if expression === value2
-            break;
-        case 'attendance':
-            report.style.display = 'none';
-            comments.style.display = 'none';
-            fees.style.display = 'none';
-            petition.style.display = 'none';
-            suggestion.style.display = 'none';
-            dashboard.style.display = 'none';
-            grade.style.display = 'none';
-            attendance.style.display = 'block';
-            // code to execute if expression === value2
-            break;
-        case 'comments':
-            report.style.display = 'none';
-            attendance.style.display = 'none';
-            fees.style.display = 'none';
-            petition.style.display = 'none';
-            suggestion.style.display = 'none';
-            dashboard.style.display = 'none';
-            grade.style.display = 'none';
-            comments.style.display = 'block';
-            // code to execute if expression === value2
-            break;
-        case 'fees':
-            report.style.display = 'none';
-            attendance.style.display = 'none';
-            comments.style.display = 'none';
-            fees.style.display = 'none';
-            petition.style.display = 'none';
-            suggestion.style.display = 'none';
-            dashboard.style.display = 'none';
-            grade.style.display = 'none';
-            fees.style.display = 'block';
-            // code to execute if expression === value2
-            break;
-        case 'petition':
-            report.style.display = 'none';
-            attendance.style.display = 'none';
-            comments.style.display = 'none';
-            fees.style.display = 'none';
-
-            suggestion.style.display = 'none';
-            dashboard.style.display = 'none';
-            grade.style.display = 'none';
-            petition.style.display = 'block';
-            // code to execute if expression === value2
-            break;
-        case 'suggestion':
-            report.style.display = 'none';
-            attendance.style.display = 'none';
-            comments.style.display = 'none';
-            fees.style.display = 'none';
-            petition.style.display = 'none';
-
-            dashboard.style.display = 'none';
-            grade.style.display = 'none';
-            suggestion.style.display = 'block';
-            // code to execute if expression === value2
-            break;
-        // ... more cases
-        default:
-        // code to execute if no case matches
-    }
+//     // Add click event listener to the toggleDiv
+//     // toggleDiv.addEventListener('click', function () {
+//     // Toggle visibility of firstDiv and secondDiv
 
 
-    // if (firstDiv.style.display === 'none') {
-    //     firstDiv.style.display = 'block';
-    //     secondDiv.style.display = 'none';
-    // } else {
-    //     firstDiv.style.display = 'none';
-    //     secondDiv.style.display = 'block';
-    // }
-    // });
+//     switch (section) {
+//         case 'dashboard':
+//             grade.setAttribute("hidden", true);
+//             report.setAttribute("hidden", true);
+//             attendance.setAttribute("hidden", true);
+//             comments.setAttribute("hidden", true);
+//             fees.setAttribute("hidden", true);
+//             petition.setAttribute("hidden", true);
+//             suggestion.setAttribute("hidden", true);
+//             dashboard.removeAttribute("hidden");
+//             break;
+//         case 'grades':
+//             report.setAttribute("hidden", true);
+//             attendance.setAttribute("hidden", true);
+//             comments.setAttribute("hidden", true);
+//             fees.setAttribute("hidden", true);
+//             petition.setAttribute("hidden", true);
+//             suggestion.setAttribute("hidden", true);
+//             dashboard.setAttribute("hidden", true);
+//             grade.removeAttribute("hidden");
+//             break;
+//         case 'report':
+//             grade.setAttribute("hidden", true);
+//             attendance.setAttribute("hidden", true);
+//             comments.setAttribute("hidden", true);
+//             fees.setAttribute("hidden", true);
+//             petition.setAttribute("hidden", true);
+//             suggestion.setAttribute("hidden", true);
+//             dashboard.setAttribute("hidden", true);
+//             report.removeAttribute("hidden");
+//             break;
+//         case 'attendance':
+//             report.setAttribute("hidden", true);
+//             comments.setAttribute("hidden", true);
+//             fees.setAttribute("hidden", true);
+//             petition.setAttribute("hidden", true);
+//             suggestion.setAttribute("hidden", true);
+//             dashboard.setAttribute("hidden", true);
+//             grade.setAttribute("hidden", true);
+//             attendance.removeAttribute("hidden");
+//             break;
+//         case 'comments':
+//             report.setAttribute("hidden", true);
+//             attendance.setAttribute("hidden", true);
+//             fees.setAttribute("hidden", true);
+//             petition.setAttribute("hidden", true);
+//             suggestion.setAttribute("hidden", true);
+//             dashboard.setAttribute("hidden", true);
+//             grade.setAttribute("hidden", true);
+//             comments.removeAttribute("hidden");
+//             break;
+//         case 'fees':
+//             report.setAttribute("hidden", true);
+//             attendance.setAttribute("hidden", true);
+//             comments.setAttribute("hidden", true);
+//             fees.setAttribute("hidden", true);
+//             petition.setAttribute("hidden", true);
+//             suggestion.setAttribute("hidden", true);
+//             dashboard.setAttribute("hidden", true);
+//             grade.setAttribute("hidden", true);
+//             fees.removeAttribute("hidden");
+//             break;
+//         case 'petition':
+//             report.setAttribute("hidden", true);
+//             attendance.setAttribute("hidden", true);
+//             comments.setAttribute("hidden", true);
+//             fees.setAttribute("hidden", true);
+//             suggestion.setAttribute("hidden", true);
+//             dashboard.setAttribute("hidden", true);
+//             grade.setAttribute("hidden", true);
+//             petition.removeAttribute("hidden");
+//             break;
+//         case 'suggestion':
+//             report.setAttribute("hidden", true);
+//             attendance.setAttribute("hidden", true);
+//             comments.setAttribute("hidden", true);
+//             fees.setAttribute("hidden", true);
+//             petition.setAttribute("hidden", true);
+//             dashboard.setAttribute("hidden", true);
+//             grade.setAttribute("hidden", true);
+//             suggestion.removeAttribute("hidden");
+//             break;
+//         // ... more cases
+//         default:
+//         // code to execute if no case matches
+//     }
 
 
-    // Navigate to the provided URL
-    // window.location.href = url;
-}
+//     // if (firstDiv.style.display === 'none') {
+//     //     firstDiv.style.display = 'block';
+//     //     secondDiv.style.display = 'none';
+//     // } else {
+//     //     firstDiv.style.display = 'none';
+//     //     secondDiv.style.display = 'block';
+//     // }
+//     // });
+
+
+//     // Navigate to the provided URL
+//     // window.location.href = url;
+// }
 /////////////////////////////////////////////////////
 
 
